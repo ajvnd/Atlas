533bfd98bbd3b0e3ae73e6599ae796ed3e53c03b
Ahmad <ahmadjavadi17@gmail.com>
2023-07-23 15:51:10 +0330

    Remove cookie-based authentication, and adjust routing
    Removed lines of code responsible for cookie-based authentication because the application will now handle authentication via JWT tokens instead. Also adjusted the default routing to point to the Home controller by default for improved UX, as users were confused by being redirected to the SignIn page. Made minor reordering of code for better readability and maintainability.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 using Atlas.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 // Add services
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
@@ -14,24 +12,19 @@ builder.Services.AddDbContext<AtlasDbContext>((c) =>
     c.UseLazyLoadingProxies().UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.SlidingExpiration = true;
-        options.ExpireTimeSpan = TimeSpan.FromDays(3);
-        options.LoginPath = "/Authentication/SignIn";
-    });
-
-
 // Add Middlewares
+var app = builder.Build();
 
 using var serviceScope = app.Services.CreateScope();
 using var dbContext = serviceScope.ServiceProvider.GetRequiredService<AtlasDbContext>();
 
- dbContext.Database.EnsureCreated();
+dbContext.Database.EnsureCreated();
+
+app.UseStaticFiles();
+app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Authentication}/{action=SignIn}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
