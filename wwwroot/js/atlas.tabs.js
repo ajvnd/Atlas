39541f5b48c9f0bfa055a2df2e28b53766018010
ahmad <ahmadjavadi17@gmail.com>
2023-08-19 14:53:38 +0330

    tab contents

---
+++ wwwroot/js/atlas.tabs.js
@@ -0,0 +1,82 @@
+let products = [{
+    label: {
+        text: 'استان'
+    },
+    editorType: 'dxTagBox',
+},
+    {
+        label: {
+            text: 'هویت توسعه دهنده'
+        },
+        editorType: 'dxTagBox',
+    },
+    {
+        label: {
+            text: 'دسته کاربردی'
+        },
+        editorType: 'dxTagBox',
+    },
+    {
+        label: {
+            text: 'نوع خدمات'
+        },
+        editorType: 'dxTagBox',
+    },
+    {
+        label: {
+            text: 'وضعیت'
+        },
+        editorType: 'dxSwitch',
+        editorOptions: {}
+    }
+];
+
+let researchers = [{
+    label: {
+        text: 'استان'
+    },
+    editorType: 'dxTagBox',
+},
+    {
+        label: {
+            text: 'دانشگاه'
+        },
+        editorType: 'dxTextBox',
+    },
+];
+
+let organizations = [
+    {
+        label: {
+            text: 'استان'
+        },
+        editorType: 'dxTagBox',
+    },
+    {
+        label: {
+            text: 'مقطع تحصیلی'
+        },
+        editorType: 'dxTagBox',
+    }
+];
+
+let companies = [
+    {
+        label: {
+            text: 'استان'
+        },
+        editorType: 'dxTagBox',
+    },
+    {
+        label: {
+            text: 'وضعیت دانش بنیانی'
+        },
+        editorType: 'dxTagBox',
+    },
+    {
+        label: {
+            text: 'توانمندی ها'
+        },
+        editorType: 'dxTagBox',
+    },
+];
