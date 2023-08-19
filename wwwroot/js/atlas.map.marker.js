2d81d55b746d0c7b424872cf8c6a3310548221c9
ahmad <ahmadjavadi17@gmail.com>
2023-08-19 14:25:22 +0330

    map markers were added.

---
+++ wwwroot/js/atlas.map.marker.js
@@ -0,0 +1,20 @@
+let markersArray = [
+    {
+        coordinates: [51.047885, 34.71912],
+        attributes: {
+            name: 'قم',
+        }
+    },
+    {
+        coordinates: [51.552348, 35.57446],
+        attributes: {
+            name: 'تهران',
+        }
+    },
+    {
+        coordinates: [59.021837, 35.74464],
+        attributes: {
+            name: 'خراسان رضوی',
+        }
+    }
+];
