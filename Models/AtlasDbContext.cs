a29e7728531be60e2124cc4a0ceb4bc81510d2a3
ahmad <ahmadjavadi17@gmail.com>
2023-09-09 13:07:37 +0330

    change

--- Models/AtlasDbContext.cs
+++ Models/AtlasDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 
 namespace Atlas.Models;
 
@@ -10,12 +12,17 @@ public class AtlasDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
     public DbSet<Province> Provinces { get; set; }
+    public DbSet<Domain> Domains { get; set; }
+    public DbSet<Contract> Contracts { get; set; }
 }
 
 public class BaseEntity
 {
     public int Id { get; set; }
     public DateTime ModifiedDate { get; set; }
+
+    [NotMapped]
+    public string PersianModifiedDate => ModifiedDate.ToString("H:mm yyyy/MM/dd", CultureInfo.GetCultureInfo("fa-IR"));
 }
 
 public class User : BaseEntity
@@ -28,4 +35,19 @@ public class Province : BaseEntity
 {
     public string PersianTitle { get; set; }
     public string EnglishTitle { get; set; }
+}
+
+public class Domain : BaseEntity
+{
+    public string Title { get; set; }
+}
+
+public class Contract : BaseEntity
+{
+    public string Title { get; set; }
+}
+
+public class Resume
+{
+    public string Url { get; set; }
 }
