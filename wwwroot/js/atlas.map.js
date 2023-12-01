64dd4cfcc5bf5d8fdf8431c3102b71f7d1f57cc5
ahmad <ahmadjavadi17@gmail.com>
2023-12-01 16:26:36 +0330

    changes.

--- wwwroot/js/atlas.map.js
+++ wwwroot/js/atlas.map.js
@@ -1,27 +1,66 @@
 $(function () {
-    $(".body-left-content").dxVectorMap(
-        {
-            zoomFactor:2.5,
-            width:"100%",
-            height:"100%",
-            bounds: [43.13321914472885, 50.83717618250027, 61.96, 8.33],
-            tooltip: {
-                enabled: true,
-                customizeTooltip(arg) {
-                    if (arg.layer.type === 'marker') {
-                        return { text: arg.attribute('name') };
-                    }
-                    return null;
-                },
-            },
-            layers:[{
-                name: 'areas',
-                dataSource: DevExpress.viz.map.sources.eurasia
-            },
-                {
-                    dataSource:markersArray,
-                }],
-            controlBar:{enabled:false},
+    DevExpress.config({
+        floatingActionButtonConfig: {
+            position: {
+                my: "right bottom",
+                at: "right bottom",
+                of: ".body-left",
+                offset: "-16 -16"
+            }
         }
-    );
+    });
+    $(".map-switch").dxSpeedDialAction({
+        hint: "Edit",
+        icon: "map",
+
+        onClick: function (e) {
+            let icon = e.component.option('icon')
+            let leftContent = $(".body-left-content");
+
+            if (icon ==='map'){
+                leftContent.dxVectorMap('instance').dispose();   
+            }else{
+                leftContent.dxDataGrid('instance').dispose();
+            }
+            
+            let iconRes = icon === 'map' ? 'smalliconslayout' : 'map';
+            e.component.option('icon', iconRes)
+
+            if (iconRes === 'map') {
+                leftContent.dxVectorMap(vectorMap);
+            } else {
+                leftContent.dxDataGrid(grid)
+            }
+        }
+    });
+
+
+    let vectorMap = {
+        zoomFactor: 2.5,
+        width: "100%",
+        height: "100%",
+        bounds: [43.13321914472885, 50.83717618250027, 61.96, 8.33],
+        tooltip: {
+            enabled: true,
+            customizeTooltip(arg) {
+                if (arg.layer.type === 'marker') {
+                    return {text: arg.attribute('name')};
+                }
+                return null;
+            },
+        },
+        layers: [{
+            name: 'areas',
+            dataSource: DevExpress.viz.map.sources.eurasia
+        },
+            {
+                dataSource: markersArray,
+            }],
+        controlBar: {enabled: false},
+    };
+
+    let grid = {}
+
+    $(".body-left-content").dxVectorMap(vectorMap);
+
 });
